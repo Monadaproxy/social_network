a781cceccb5a197d3b0c08da89c1119ce1f7c3d5
Monadaproxy <vdovin.stepa2015@gmail.com>
2025-06-28 14:49:22 +0500

    Сборка в Docker II

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,6 +15,8 @@ then
 fi
 
 
+python manage.py makemigrations
+
 python manage.py migrate
 
 echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@example.com', 'admin1234') if not User.objects.filter(username='admin').exists() else None" | python manage.py shell
--- users/models.py
+++ users/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    avatar = models.ImageField(upload_to='avatars/', blank=True, null=True)
+    avatar = models.ImageField(upload_to='avatars/', blank=True, default='avatars/default_avatar.png')
     bio = models.TextField(max_length=60, blank=True, null=True)
     city = models.CharField(default='Moscow', max_length=30)
 
